bfa5979f061fcb2a4cc7a881584308d05c3305d8
David SK app developer <david@smarterkey.com>
2013-03-04 23:32:22 +0100

    Initial Commit

---
+++ SK1-2/SKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SKAppDelegate.h
+//  SK1-2
+//
+//  Created by Administrator on 3/4/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SK1-2/SKViewController.h
@@ -0,0 +1,13 @@
+//
+//  SKViewController.h
+//  SK1-2
+//
+//  Created by Administrator on 3/4/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKViewController : UIViewController
+
+@end
